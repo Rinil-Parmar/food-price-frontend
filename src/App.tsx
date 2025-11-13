4dfd0bd81a3a861ef51bb54ffe515c637eb4232b
Rinil-Parmar <parmarrinila2002@gmail.com>
2025-11-13 17:20:03 -0500

    init frontend and added tailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className="flex items-center justify-center h-screen bg-gray-900 text-white text-2xl font-bold">
+        Tailwind CSS is working 🎉
+      </div>
+  );
+}
+
+export default App;
